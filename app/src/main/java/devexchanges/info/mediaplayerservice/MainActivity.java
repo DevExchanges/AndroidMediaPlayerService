b18bdbf3f49963f259beb71616d9c34eebbd4c4b
hongthaiis <hongthaiis92@gmail.com>
2015-12-12 11:01:35 +0700

    first commit

---
+++ app/src/main/java/devexchanges/info/mediaplayerservice/MainActivity.java
@@ -0,0 +1,47 @@
+package devexchanges.info.mediaplayerservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View btnStart;
+    private View btnStop;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlayer(v);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlayer(v);
+            }
+        });
+    }
+
+    public void startPlayer(View v) {
+        Intent i = new Intent(this, MediaPlayerService.class);
+        i.putExtra(MediaPlayerService.EXTRA_PLAYLIST, "main");
+        i.putExtra(MediaPlayerService.EXTRA_SHUFFLE, true);
+        startService(i);
+    }
+
+    public void stopPlayer(View v) {
+        stopService(new Intent(this, MediaPlayerService.class));
+    }
+
+}
---
+++ app/src/main/java/devexchanges/info/mediaplayerservice/MediaPlayerService.java
@@ -0,0 +1,44 @@
+package devexchanges.info.mediaplayerservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MediaPlayerService extends Service {
+    public static final String EXTRA_PLAYLIST = "EXTRA_PLAYLIST";
+    public static final String EXTRA_SHUFFLE = "EXTRA_SHUFFLE";
+    private boolean isPlaying = false;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String playlist = intent.getStringExtra(EXTRA_PLAYLIST);
+        boolean useShuffle = intent.getBooleanExtra(EXTRA_SHUFFLE, false);
+        play(playlist, useShuffle);
+        return (START_NOT_STICKY);
+    }
+
+    @Override
+    public void onDestroy() {
+        stop();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return (null);
+    }
+
+    private void play(String playlist, boolean useShuffle) {
+        if (!isPlaying) {
+            Log.w(getClass().getName(), "Got to play()!");
+            isPlaying = true;
+        }
+    }
+
+    private void stop() {
+        if (isPlaying) {
+            Log.w(getClass().getName(), "Got to stop()!");
+            isPlaying = false;
+        }
+    }
+}
